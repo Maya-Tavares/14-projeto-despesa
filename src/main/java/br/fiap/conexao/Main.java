899537ec6863e62c6f5cd24090746df7d77ef5e4
Maya Tavares <tavares.pereira@aluno.ifsp.edu.br>
2025-08-18 20:51:38 -0300

    Update Main.java

--- src/main/java/br/fiap/conexao/Main.java
+++ src/main/java/br/fiap/conexao/Main.java
@@ -3,10 +3,17 @@ package br.fiap.conexao;
 import br.fiap.modelo.Categoria;
 import br.fiap.modelo.CategoriaDAO;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         CategoriaDAO dao = new CategoriaDAO();
-        Categoria categoria = new Categoria(8L, "festa");
-        dao.inserir(categoria);
+//        Categoria categoria = new Categoria(2L, "auto");
+//        dao.inserir(categoria);
+        List<Categoria> list = dao.listar();
+        for(Categoria categoria : list){
+            System.out.println(categoria.getId() + " ----> " + categoria.getCategoria());
+        }
     }
 }
