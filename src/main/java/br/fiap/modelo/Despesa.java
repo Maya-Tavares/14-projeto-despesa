6b0f2ec4e8ae239a6bf8974e3a546f5ad4b26a7b
Maya Tavares <tavares.pereira@aluno.ifsp.edu.br>
2025-08-18 20:52:00 -0300

    Create Despesa.java

---
+++ src/main/java/br/fiap/modelo/Despesa.java
@@ -0,0 +1,51 @@
+package br.fiap.modelo;
+
+import java.time.LocalDate;
+
+public class Despesa {
+    private long id;
+    private double valor;
+    private LocalDate data;
+    private String descricao;
+    private Categoria categoria;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+}
