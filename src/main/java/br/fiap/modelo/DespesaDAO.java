ec3c0499e9cf37c7f617230f8bf59893ea081858
Maya Tavares <tavares.pereira@aluno.ifsp.edu.br>
2025-08-25 20:57:26 -0300

    Update DespesaDAO.java

--- src/main/java/br/fiap/modelo/DespesaDAO.java
+++ src/main/java/br/fiap/modelo/DespesaDAO.java
@@ -12,10 +12,10 @@ public class DespesaDAO {
     private String sql;
 
     public void inserir(Despesa despesa) {
-        sql = "insert into java_despesa values (?, ?, ?, ?, ?)";
+        sql = "insert into java_despesa values (seqd.nextval, ?, ?, ?, ?)";
         try(Connection connection = Conexao.conectar()) {
             ps = connection.prepareStatement(sql);
-            ps.setLong(1, despesa.getId());
+//            ps.setLong(1, despesa.getId());
             ps.setString(2, despesa.getDescricao());
             ps.setDouble(3, despesa.getValor());
             ps.setDate(4, Date.valueOf(despesa.getData()));
